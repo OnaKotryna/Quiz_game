fd876f94088412565632535cd4ba2fe846416532
Ona Kotryna <ok.kuncyte@gmail.com>
2023-09-11 17:48:56 +0300

    Load UI module in main file

--- main.py
+++ main.py
@@ -2,27 +2,28 @@ from data import load_questions
 from game import Game
 import os
 
-os.system('clsf')
-print("willkommen")
+from ui import QuizInterface
+
 
 game = Game(load_questions())
+quiz_ui = QuizInterface(game)
 
-while game.have_question():
-    question = game.get_question()
-    if question:
-        answ = input(
-            f"Q{game.question_num}: {question.question} True or False? \n")
-        game.check_answer(question, answ)
+# while game.have_question():
+#     question = game.get_question()
+#     if question:
+#         answ = input(
+#             f"Q{game.question_num}: {question.question} True or False? \n")
+#         game.check_answer(question, answ)
 
-print('*******************************')
-print(f'Your score: {game.score}/{game.question_num} ')
-print('Answers:')
-num = 1
-for q in game.questions:
-    print(f'Q{num}: {q.question}')
-    num += 1
-    if q.guessed:
-        print(f'Correct. Answer: {q.answer}')
-    else:
-        print(f'Incorrect. Answer: {q.answer}')
-    print('*******************************')
+# print('*******************************')
+# print(f'Your score: {game.score}/{game.question_num} ')
+# print('Answers:')
+# num = 1
+# for q in game.questions:
+#     print(f'Q{num}: {q.question}')
+#     num += 1
+#     if q.guessed:
+#         print(f'Correct. Answer: {q.answer}')
+#     else:
+#         print(f'Incorrect. Answer: {q.answer}')
+#     print('*******************************')
